2bfc4af7c476846f9577c1b748d3fda8c2b5a281
torian12321 <torian12321@users.noreply.github.com>
2022-12-20 22:04:23 +0000

    deploy: b9ed856a8666da89458a8bdbbe98ece78f128a6a

--- src/state/selectors/bill-selectors.ts
+++
@@ -1,8 +0,0 @@
-import type { BillId } from '../../types/legislations'
-import type { RootState } from '../store'
-
-export const getAllBills = (state: RootState) => state.bills.bills
-export const getBillById = (state: RootState, id: BillId) => {
-  const bills = getAllBills(state)
-  return bills.find(bill => bill.number === id)
-}
