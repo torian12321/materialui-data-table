7f69cd8c42d88e9cf5c325673dde71f9da5a54d4
Aitor <aitorpalomares@hotmail.com>
2022-12-20 16:14:01 +0100

    add options tests

--- src/state/slices/options-slice.ts
+++ src/state/slices/options-slice.ts
@@ -3,16 +3,16 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { BillId } from '../../types/legislations'
 import type { RootState } from '../store'
 
-export interface BillsState {
+export interface OptionsState {
   selectedBill?: BillId // bill number ID
   tablePage: number
   rowsPerPage: number
 }
 
-const initialState: BillsState = {
+export const initialState: OptionsState = {
   selectedBill: undefined,
   tablePage: 0,
-  rowsPerPage: 5
+  rowsPerPage: 5,
 }
 
 export const optionsSlice = createSlice({
