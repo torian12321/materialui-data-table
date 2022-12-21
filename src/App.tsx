1509512a08fadbe90007347e3d58fcc32f99078f
Aitor <aitorpalomares@hotmail.com>
2022-12-21 11:39:44 +0100

    show filtered table on main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
+import { useState } from 'react'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
 import { useFetchBills } from './hooks/use-fetch-bills'
 import { BillsTable } from './ui/components/bills-table'
 import { ModalDetails } from './ui/components/modal-details'
 import Filters from './ui/layout/filters'
+import { useAppSelector } from './state/hooks'
+import { getFilteredBills, getFilteredFavoriteBills } from './state/selectors/bill-selectors'
+import TabPanel from './ui/components/tabs-panel'
 import './App.css'
 
 const App = () => {
   useFetchBills()
+  const [tabIndex, setTabIndex] = useState(0);
+  const bills = useAppSelector(getFilteredBills)
+  const billsFavorites = useAppSelector(getFilteredFavoriteBills)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabIndex(newValue);
+  }
 
   return (
     <div className="app">
       <div className="app-body">
         <Filters />
         <ModalDetails />
-        <BillsTable />
+
+        <Tabs value={tabIndex} onChange={handleChange} aria-label="table-tabs">
+          <Tab label="All Bills" />
+          <Tab label={`Favorite Bills (${billsFavorites.length})`} />
+        </Tabs>
+        <TabPanel value={tabIndex} index={0}>
+          <BillsTable bills={bills} />
+        </TabPanel>
+        <TabPanel value={tabIndex} index={1}>
+          <BillsTable bills={billsFavorites} />
+        </TabPanel>
       </div>
     </div>
   )
--- src/ui/components/bills-table/index.tsx
+++ src/ui/components/bills-table/index.tsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../../state/hooks'
-import { getFilteredBills, getFavorites } from '../../../state/selectors/bill-selectors'
+import { getFavorites } from '../../../state/selectors/bill-selectors'
 import { selectBill } from '../../../state/slices/options-slice'
 import { addFavorite, removeFavorite } from '../../../state/slices/bill-slice'
+import type { Bill } from '../../../state/slices/bill-slice'
 
 import Table from './bills-table'
 import Pagination from './bills-table-pagination'
 import './bills-table.css'
 
-export const BillsTable = () => {
+interface Props {
+  bills: Bill[]
+}
+
+export const BillsTable = ({ bills }: Props) => {
   const dispatch = useAppDispatch()
-  const bills = useAppSelector(getFilteredBills)
   const favoriteIds = useAppSelector(getFavorites)
   const [currentPage, setCurrentPage] = useState(0)
   const [rowsPerPage, setRowsPerPage] = useState(10)
